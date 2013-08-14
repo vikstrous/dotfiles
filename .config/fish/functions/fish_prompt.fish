c284e2e98eb59ca0baa5c6f7eefa9f6975ec6d64
Viktor Stanchev <me@viktorstanchev.com>
2013-08-14 08:38:12 -0400

    make fish_prompt compatible with python 3 and kill cpu

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,11 +5,11 @@ function fish_prompt
     set_color cyan
     echo -n (date "+[%H:%M:%S]")
     echo -n " "
+#    set_color white
+#    cat /tmp/cpu_usage
+#    echo -n " "
     set_color white
-    cat /tmp/cpu_usage
-    echo -n " "
-    set_color white
-    echo -n (python -c 'print "%.1f" % (100.0*(1-'(cat /proc/meminfo | head -n 2 | tail -1 | awk '{print $2}')'.0/'(cat /proc/meminfo | head -1 | awk '{print $2}')'))')
+    echo -n (python -c 'print("%.1f" % (100.0*(1-'(cat /proc/meminfo | head -n 2 | tail -1 | awk '{print $2}')'.0/'(cat /proc/meminfo | head -1 | awk '{print $2}')')))')
     echo -n " "
     set_color yellow
     echo (pwd)
