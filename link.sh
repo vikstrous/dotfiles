4963a7ae1dc985764638be5b3c8e0cd828af48dc
Viktor Stanchev <me@viktorstanchev.com>
2013-08-15 01:56:47 -0400

    caps lock and fast repeat

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@ rm ~/.vim
 ln -s $dotfiles/.vim ~/.vim
 rm ~/.if
 ln -s $dotfiles/.if ~/.if
+rm ~/.remap_caps_lock
+ln -s $dotfiles/.remap_caps_lock ~/.remap_caps_lock
 rm ~/.vimrc
 ln -s $dotfiles/.vimrc ~/.vimrc
 rm ~/.xmonad/xmonad.hs
