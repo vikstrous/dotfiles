5873f4188a73333d94a87168315275cc5041ce67
Viktor Stanchev <me@viktorstanchev.com>
2013-08-15 00:52:43 -0400

    fish git

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -11,8 +11,11 @@ function fish_prompt
     set_color white
     echo -n (python -c 'print("%.1f" % (100.0*(1-'(cat /proc/meminfo | head -n 2 | tail -1 | awk '{print $2}')'.0/'(cat /proc/meminfo | head -1 | awk '{print $2}')')))')
     echo -n " "
-    set_color yellow
-    echo (pwd)
+    set_color $fish_color_cwd
+    echo -n (pwd)
+    set_color white
+    set last_status $status
+    printf '%s\n' (__fish_git_prompt)
     set_color green
     echo -n '><> '
     set_color normal
