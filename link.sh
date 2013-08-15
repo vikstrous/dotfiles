82915819ddcd2720cfb8d53e9e041e2ee4f45c8f
Viktor Stanchev <me@viktorstanchev.com>
2013-08-15 01:43:46 -0400

    xmonad

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@ set dotfiles ~/dev/dotfiles
 ln -s $dotfiles/.vim ~/.vim
 ln -s $dotfiles/.if ~/.if
 ln -s $dotfiles/.vimrc ~/.vimrc
+ln -s $dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s $dotfiles/.config/fish/functions ~/.config/fish/functions
 ln -s $dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 echo OK
