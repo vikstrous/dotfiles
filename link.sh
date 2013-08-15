c1066d390dcfc643f3829dfa18ee7166ea5e0a1d
Viktor Stanchev <me@viktorstanchev.com>
2013-08-15 01:50:26 -0400

    safe to call link multiple times

--- link.sh
+++ link.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/fish
 set dotfiles ~/dev/dotfiles
+rm ~/.vim
 ln -s $dotfiles/.vim ~/.vim
+rm ~/.if
 ln -s $dotfiles/.if ~/.if
+rm ~/.vimrc
 ln -s $dotfiles/.vimrc ~/.vimrc
+rm ~/.xmonad/xmonad.hs
 ln -s $dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+rm ~/.config/fish/functions
 ln -s $dotfiles/.config/fish/functions ~/.config/fish/functions
+rm ~/.config/fish/config.fish
 ln -s $dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 echo OK
