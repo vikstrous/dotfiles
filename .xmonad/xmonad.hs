5afb3ac8eca5ea7a8243b31b7fec0e021258d1de
Viktor Stanchev <me@viktorstanchev.com>
2013-08-16 05:03:52 -0400

    run xmobar through xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,19 +2,12 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-main = do
-xmproc <- spawnPipe "/usr/bin/xmobar /home/v/.xmobarrc.hs"
-xmonad $ defaultConfig
+-- xmproc <- spawnPipe "/usr/bin/xmobar /home/v/.xmobarrc.hs"
+main = xmonad =<< xmobar defaultConfig
     { modMask = mod4Mask -- Use Super instead of Alt
     , terminal = "urxvt"
     , manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock") --mod4mask is the windows key
-    , ((0, xK_Print), spawn "gnome-screenshot")
-    ]
-
- 
+    }
--- link.fish
+++ link.fish
@@ -1,30 +1,10 @@
 #!/usr/bin/fish
 set dotfiles ~/dotfiles
 
-set array '.vim' '.if' '.Xdefaults' '.xinitrc' '.gitconfig' '.remap_caps_lock' '.vimrc' '.xmonad/xmonad.hs' '.config/fish/functions' '.config/fish/config.fish' '.xmobarrc.hs'
+set array '.vim' '.if' '.Xdefaults' '.xinitrc' '.gitconfig' '.remap_caps_lock' '.vimrc' '.xmonad/xmonad.hs' '.config/fish/functions' '.config/fish/config.fish'
 for i in $array
   rm ~/$i
   ln -s $dotfiles/$i ~/$i
 end
 
-#rm ~/.vim
-#ln -s $dotfiles/.vim ~/.vim
-#rm ~/.if
-#ln -s $dotfiles/.if ~/.if
-#rm ~/.Xdefaults
-#ln -s $dotfiles/.Xdefaults ~/.Xdefaults
-#rm ~/.xinitrc
-#ln -s $dotfiles/.xinitrc ~/.xinitrc
-#rm ~/.gitconfig
-#ln -s $dotfiles/.gitconfig ~/.gitconfig
-#rm ~/.remap_caps_lock
-#ln -s $dotfiles/.remap_caps_lock ~/.remap_caps_lock
-#rm ~/.vimrc
-#ln -s $dotfiles/.vimrc ~/.vimrc
-#rm ~/.xmonad/xmonad.hs
-#ln -s $dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-#rm ~/.config/fish/functions
-#ln -s $dotfiles/.config/fish/functions ~/.config/fish/functions
-#rm ~/.config/fish/config.fish
-#ln -s $dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 echo OK
