b0a8246ec86ccb2cd2e15fb5db0d8a7d0b4b7e85
Viktor Stanchev <me@viktorstanchev.com>
2013-08-24 01:04:03 -0400

    battery or something.. too drunnk right now

--- arch/setup.sh
+++ arch/setup.sh
@@ -112,7 +112,7 @@ sudo pacman -U yaourt-1.3-1-any.pkg.tar.xz
 
 
 ### user stuff
-pacman -S openssh firefox git fakeroot xdiskusage xev
+pacman -S openssh firefox git fakeroot xdiskusage xev acpi
 ssh-keygen # for git
 yaourt chrome
 pacman -S weechat
