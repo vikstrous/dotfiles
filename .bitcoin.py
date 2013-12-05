6e59c5d743ab93fa95248e6debc9be741302468e
Viktor Stanchev <me@viktorstanchev.com>
2013-12-05 18:46:59 -0500

    bitcoin!

--- .bitcoin.py
+++ .bitcoin.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import urllib.request, json
 
-print(float(json.loads(urllib.request.urlopen('https://mtgox.com/api/1/BTCUSD/ticker').read().decode('UTF-8'))['return']['last']['value']))
+print(float(json.loads(urllib.request.urlopen('https://data.mtgox.com/api/1/BTCUSD/ticker').read().decode('UTF-8'))['return']['last']['value']))
