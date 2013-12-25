80dc67d5666d704b715f73557fd4255dcbb98467
Viktor Stanchev <me@viktorstanchev.com>
2013-12-24 22:10:07 -0500

    move things

--- install.fish
+++ install.fish
@@ -1,14 +1,14 @@
 #!/usr/bin/fish
 
 # rsyslog
-pacman -S rsyslog
-mkdir /etc/rsyslog.d
-cp rsyslog/rsyslog.d/* /etc/rsyslog.d/
-cp rsyslog/rsyslog.conf /etc/rsyslog.conf
-systemctl daemon-reload
-systemctl enable rsyslog
-systemctl stop rsyslog
-systemctl start rsyslog
+#pacman -S rsyslog
+#mkdir /etc/rsyslog.d
+#cp rsyslog/rsyslog.d/* /etc/rsyslog.d/
+#cp rsyslog/rsyslog.conf /etc/rsyslog.conf
+#systemctl daemon-reload
+#systemctl enable rsyslog
+#systemctl stop rsyslog
+#systemctl start rsyslog
 
 # iptables
 cp iptables/laptop.conf /etc/iptables/iptables.rules
