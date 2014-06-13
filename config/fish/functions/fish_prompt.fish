0a881047c9ebc7575b3e9b909c5ff38f53c4aebe
Viktor Stanchev <me@viktorstanchev.com>
2014-06-13 18:37:36 -0400

    better fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,23 +1,12 @@
 function fish_prompt
     set savedstatus $status
-    set_color red
-    echo -n $savedstatus
-    echo -n " "
-    set_color cyan
-    echo -n (date "+[%H:%M:%S]")
-    echo -n " "
-#    set_color white
-#    cat /tmp/cpu_usage
-#    echo -n " "
-#    set_color white
-#    echo -n (python -c 'print("%.1f" % (100.0*(1-'(cat /proc/meminfo | head -n 2 | tail -1 | awk '{print $2}')'.0/'(cat /proc/meminfo | head -1 | awk '{print $2}')')))')
-#    echo -n " "
-    set_color normal
-    echo -n (pwd)
-    set_color white
-    set last_status $status
-    printf '%s\n' (__fish_git_prompt)
-    set_color green
-    echo -n '><> '
-    set_color normal
+    echo -n (set_color white)"╭─"(set_color normal)
+    if [ $savedstatus != 0 ]
+        set_color red
+        echo -n $savedstatus" "
+    end
+    echo -n (set_color cyan)(date "+[%H:%M:%S]")(set_color normal) (pwd)(set_color white)
+    __fish_git_prompt
+    echo
+    echo -n (set_color white)"╰─"(set_color green)'<")>< '(set_color normal)
 end
