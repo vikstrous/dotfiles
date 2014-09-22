fc106914a5929c669f9d28ca9cd5d799eca1b989
Viktor Stanchev <me@viktorstanchev.com>
2014-09-21 17:19:10 -0700

    fix IDEA..

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,13 +14,15 @@ set __fish_git_prompt_char_upstream_behind '↓'
 set fish_greeting (cat ~/dotfiles/if | sort -R | head -n 1)
 
 set PATH (cope_path) /home/v/work/moz-git-tools /opt/android-sdk/platform-tools /home/v/work/b2g-hamachi/B2G/tools/update-tools/bin/linux-x86 /opt/sunbird /opt/play /home/v/idea-IU-129.1525/bin /usr/local/heroku/bin $PATH /home/v/.gem/ruby/2.1.0/bin/ /opt/jdk1.6.0_45/bin /home/v/work/b2g-hamachi/B2G/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/ /home/v/Downloads/android-sdk-linux/platform-tools /home/v/.gem/ruby/2.1.0/bin
+
 set PATH $PATH (ruby -rubygems -e "puts Gem.user_dir")/bin
 
 set PATH $PATH ~/go/bin
 
-set GOPATH ~/go
+# fix IDEA
+set PATH /usr/lib/jvm/default/bin $PATH ~/go/bin
 
-set JAVA_HOME /usr/lib/jvm/default
+set GOPATH ~/go
 
 alias less 'less -R'
 alias chromium 'chromium --incognito'
