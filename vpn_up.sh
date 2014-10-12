86873f9f78bd6eebd521c1f1f256ee3eb0335066
Viktor Stanchev <me@viktorstanchev.com>
2014-10-12 11:11:16 -0700

    this time vpn actually works

--- vpn_up.sh
+++ vpn_up.sh
@@ -1,2 +1,3 @@
-openvpn --config /home/v/dotfiles/configs/tun0.conf --verb 6
-ip route add default via 10.1.0.2 dev tun0 proto static
+openvpn --config /home/v/dotfiles/configs/tun0.conf --verb 0 > /dev/null &
+sleep 1
+ip route add default via 10.1.0.1 dev tun0 proto static
