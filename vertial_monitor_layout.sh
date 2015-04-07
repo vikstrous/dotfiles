599dcc1c1addb86d0d9a83cd9a2ab6b401095bdb
Viktor Stanchev <me@viktorstanchev.com>
2015-04-07 03:54:21 -0400

    vertical monitor yay

---
+++ vertial_monitor_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --mode 1680x1050 --pos 435x0 --rotate right --output LVDS-0 --mode 1920x1080 --pos 0x1680 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
