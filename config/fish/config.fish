ee7db75738aee85277ae8c56c2014566b5281b36
Viktor Stanchev <me@viktorstanchev.com>
2015-08-31 20:30:16 -0700

    paths, unfuck chromium, switch to foxyproxy, etc.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,7 +13,7 @@ set __fish_git_prompt_char_upstream_behind '↓'
 
 set fish_greeting (host -t txt istheinternetonfire.com | cut -f 2 -d '"')
 
-set PATH (cope_path) /home/v/work/moz-git-tools /opt/android-sdk/platform-tools /home/v/work/b2g-hamachi/B2G/tools/update-tools/bin/linux-x86 /opt/sunbird /opt/play /home/v/idea-IU-129.1525/bin /usr/local/heroku/bin $PATH /home/v/.gem/ruby/2.1.0/bin/ /opt/jdk1.6.0_45/bin /home/v/work/b2g-hamachi/B2G/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/ /home/v/Downloads/android-sdk-linux/platform-tools /home/v/.gem/ruby/2.1.0/bin
+set PATH (cope_path) /home/v/work/moz-git-tools /opt/android-sdk/platform-tools /home/v/work/b2g-hamachi/B2G/tools/update-tools/bin/linux-x86 /opt/sunbird /opt/play /home/v/idea-IU-141.1532.4/bin /usr/local/heroku/bin $PATH /home/v/.gem/ruby/2.1.0/bin/ /opt/jdk1.6.0_45/bin /home/v/work/b2g-hamachi/B2G/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/ /home/v/Downloads/android-sdk-linux/platform-tools /home/v/.gem/ruby/2.1.0/bin /usr/local/bin/
 
 set PATH $PATH (ruby -rubygems -e "puts Gem.user_dir")/bin
 
@@ -26,10 +26,10 @@ set PATH /usr/lib/jvm/default/bin $PATH ~/go/bin
 
 #source /home/v/.rvm/scripts/rvm
 
-set GOPATH ~/go
+set -x GOPATH ~/go
 
 alias less 'less -R'
-alias chromium 'chromium --proxy-server=localhost:8118'
+#alias chromium 'chromium --proxy-server=localhost:8118'
 
 set -x LC_ALL 'en_CA.UTF-8'
 set -x LANG 'en_CA.UTF-8'
@@ -46,3 +46,24 @@ set -x EMSCRIPTEN_FASTCOMP "/usr/lib/emscripten-fastcomp"
 
 # add to path
 set -x PATH $PATH $EMSCRIPTEN
+
+function fuck
+    eval (thefuck $history[1])
+end
+
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+alias ...... 'cd ../../../../..'
+alias ....... 'cd ../../../../../..'
+alias ........ 'cd ../../../../../../..'
+alias ......... 'cd ../../../../../../../..'
+alias .......... 'cd ../../../../../../../../..'
+alias ........... 'cd ../../../../../../../../../..'
+alias ............ 'cd ../../../../../../../../../../..'
+alias ............. 'cd ../../../../../../../../../../../..'
+alias .............. 'cd ../../../../../../../../../../../../..'
+
+set -x PATH $PATH /usr/bin/core_perl/
+
+alias chromium 'chromium --force-device-scale-factor=1'
