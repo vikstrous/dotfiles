0165caa7f82110f6c63669f3595a987a2a2ab14b
Viktor Stanchev <me@viktorstanchev.com>
2015-10-02 00:41:17 -0700

    greething

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,7 +11,7 @@ set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
 
-set fish_greeting (host -t txt istheinternetonfire.com | cut -f 2 -d '"')
+set fish_greeting (set_color brown)-- (whoami)@(hostname) --(set_color normal)
 
 set PATH (cope_path) /home/v/work/moz-git-tools /opt/android-sdk/platform-tools /home/v/work/b2g-hamachi/B2G/tools/update-tools/bin/linux-x86 /opt/sunbird /opt/play /home/v/idea-IU-141.1532.4/bin /usr/local/heroku/bin $PATH /opt/jdk1.6.0_45/bin /home/v/work/b2g-hamachi/B2G/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/ /home/v/Downloads/android-sdk-linux/platform-tools /home/v/.gem/ruby/2.1.0/bin /usr/local/bin/
 
